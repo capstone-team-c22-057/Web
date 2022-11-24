90d11e0dba5aade42b9042b3482db2d7b902ab77
Dafa Arif Nurkholis <110544931+Krafolis@users.noreply.github.com>
2022-11-25 00:03:10 +0700

    Tambah Fungsi Gambar dan Fungsi Hapus

--- main.js
+++ main.js
@@ -7,8 +7,18 @@ const plantHealthDisplay = document.getElementById('plant-health-display');
 
 const fruitFreshnessImageDetection = document.getElementById('fruit-freshness-image-detection');
 const plantHealthImageDetection = document.getElementById('plant-health-image-detection');
-const detectionResultContainer = document.getElementById('detection-result-container');
+const fruitDetectionResult = document.getElementById('fruit-detection-result');
+const plantDetectionResult = document.getElementById('plant-detection-result');
+const plantImageResult = document.getElementById('plant-image-result');
+const fruitImageResult = document.getElementById('fruit-image-result');
 
+const fruitFreshnessImageDelete = document.getElementById('fruit-freshness-image-delete');
+const plantHealthImageDelete = document.getElementById('plant-health-image-delete');
+const selectedFruitImage = document.getElementById('selected-fruit-image');
+const selectedPlantImage = document.getElementById('selected-plant-image');
+
+
+//Fungsi Pemilihan Model
 fruitFreshnessSelection.addEventListener('click', function () {
     fruitFreshnessDisplay.removeAttribute('hidden');
     fruitFreshnessSelection.setAttribute('style', 'background-color: whitesmoke;');
@@ -27,10 +37,30 @@ plantHealthSelection.addEventListener('click', function () {
     fruitFreshnessSelection.removeAttribute('style');
 });
 
+
+//Fungsi Deteksi
 fruitFreshnessImageDetection.addEventListener('click', function () {
-    detectionResultContainer.removeAttribute('hidden');
+    fruitDetectionResult.removeAttribute('hidden');
+    plantDetectionResult.setAttribute('hidden', true);
+
+    const fruitImage = selectedFruitImage.getAttribute('src');
+    fruitImageResult.setAttribute('src', fruitImage);
 });
 
 plantHealthImageDetection.addEventListener('click', function () {
-    detectionResultContainer.removeAttribute('hidden');
+    plantDetectionResult.removeAttribute('hidden');
+    fruitDetectionResult.setAttribute('hidden', true);
+
+    const plantImage = selectedPlantImage.getAttribute('src');
+    plantImageResult.setAttribute('src', plantImage);
+});
+
+
+//Fungsi Hapus
+fruitFreshnessImageDelete.addEventListener('click', function () {
+    selectedFruitImage.removeAttribute('src');
+});
+
+plantHealthImageDelete.addEventListener('click', function () {
+    selectedPlantImage.removeAttribute('src');
 });
